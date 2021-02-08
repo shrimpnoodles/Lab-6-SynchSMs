32e9680ca32726331e3337304007a2da944285db
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-02-07 16:54:25 -0800

    Update hcier001_lab6_part2.c

--- turnin/hcier001_lab6_part2.c
+++ turnin/hcier001_lab6_part2.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *	
+ *	submission link: https://drive.google.com/file/d/1qkiuhe6rQxTpprHd7_lFum_v62OmuE9i/view?usp=sharing
  */
 
 #include <avr/io.h>
